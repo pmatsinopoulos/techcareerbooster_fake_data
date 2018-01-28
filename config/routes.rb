8de36a2442eb8bc34c2609a5bbf208b5ae28c3f6
Panayotis Matsinopoulos <Panayotis Matsinopoulos panayotis@matsinopoulos.gr>
2018-01-28 08:27:14 +0200

    Added the routes to support the students api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :exercises_and_code do
+      resources :students, only: :index, defaults: {format: :json}
+    end
+  end
 end
